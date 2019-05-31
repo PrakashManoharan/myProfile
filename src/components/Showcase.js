a558927587374fb21fdccee86081254c77595f19
JKBC1800 <31428649+JKBC1800@users.noreply.github.com>
2019-05-31 12:50:29 +1000

    Enter

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -12,10 +12,10 @@ export default class Showcase extends Component {
       <div className="showcase">
         <div className="showcase-content">
           <h2>
-            <span className="blue-text head-01">Front-End</span>
-            <span className="blue-text text-lighten-4 head-02">Developer</span>
+            <span className="head-01">Front-End</span>
+            <span className="head-02">Developer</span>
           </h2>{' '}
-          <h5 className="blue-text text-lighten-4 center-align">
+          <h5 className="center-align">
             Embracing new web technologies to develop highly secured web
             applications
           </h5>
